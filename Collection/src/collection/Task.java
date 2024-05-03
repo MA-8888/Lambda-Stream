7a01ea39949cacb274dfa680deba64f0410972c6
YukiNoguchi <126850727+MA-8888@users.noreply.github.com>
2024-05-03 17:59:26 +0900

    Collection

---
+++ Collection/src/collection/Task.java
@@ -0,0 +1,23 @@
+package collection;
+
+import java.time.LocalDate;
+
+class Task implements Comparable<Task> {
+	private LocalDate date;
+	private String description;
+
+	public Task(LocalDate date, String description) {
+		this.date = date;
+		this.description = description;
+	}
+
+	@Override
+	public int compareTo(Task other) {
+		return this.date.compareTo(other.date);
+	}
+
+	@Override
+	public String toString() {
+		return date + ": " + description;
+	}
+}
